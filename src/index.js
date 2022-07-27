155fae8ebef8d806803ec2d22e1ade44d9ed1de1
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-07-27 10:17:32 +0100

    Updating index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-//import './style.css';
+import './style.css';
 
 class Player {
-  constructor (name, score) {
+  constructor(name, score) {
     this.name = name;
     this.score = score;
   }
 }
+let scoresList;
 
-const scoresList = [] || JSON.parse(localStorage.getItem('items'));
+if (localStorage.length === 0) {
+  scoresList = [];
+} else {
+  scoresList = JSON.parse(localStorage.getItem('items'));
+}
 
 const listScores = document.querySelector('.listScores');
 
@@ -15,11 +20,17 @@ const addToDom = (name, score) => {
   const player = document.createElement('div');
   player.innerHTML += `<p>${name}: ${score}</p>`;
   listScores.appendChild(player);
-}
+};
 
 const getPlayerFromStorage = () => {
-
-}
+  if (localStorage.length) {
+    const localItem = localStorage.getItem('items');
+    const items = JSON.parse(localItem);
+    items.forEach((element) => {
+      addToDom(element.name, element.score);
+    });
+  }
+};
 
 const addNewScore = (name, score) => {
   addToDom(name, score);
@@ -30,17 +41,14 @@ const addNewScore = (name, score) => {
 
 const names = document.querySelector('.name');
 const score = document.querySelector('.score');
-const submit = document.querySelector('.submit')
+const submit = document.querySelector('.submit');
 
 submit.addEventListener('click', () => {
   if (names.value !== '' && score.value !== '') {
-    addNewScore (names.value, score.value);
+    addNewScore(names.value, score.value);
     names.value = null;
     score.value = null;
   }
 });
 
 window.addEventListener('load', getPlayerFromStorage);
-
-//still need to work on the local storage 
-//and the refresh button
