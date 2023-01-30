f6e0dfeddba6595814ffa8e1fbd18dffe8af3337
Anasse LAARABI <contact.laarabi@gmail.com>
2023-01-30 01:03:14 +0100

    Update index.js && index.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-// import './style.css';
-import './newStyle.css';
-import './scss/styles.scss'
+import './style.css';
+import('./scss/styles.scss');
 import postPlayer from './__modules__/api.js';
 import userProfile from './img/149071.png';
 import profile_1 from './img/profile1.jpg';
 import profile_2 from './img/profile2.jpg';
 import profile_3 from './img/profile3.jpg';
 
-const legendsProfile= [profile_1, profile_2, profile_3];
+const profile= [profile_1, profile_2, profile_3, userProfile];
 
 const names = document.querySelector('.name');
 const score = document.querySelector('.score');
@@ -17,35 +16,27 @@ const legendSection = document.querySelector('.legend');
 
 const legend = (arr) => {
   
-  // console.log(arr);
   legendSection.innerHTML = '';
   arr.forEach((element, i) => {
     const firstThree = document.createElement('ul');
 
     firstThree.classList.add('legendList');
-    firstThree.classList.add('hidden');
     firstThree.innerHTML += `
-    <li><img src="${legendsProfile[i]}" alt="user profile"></li>
+    <li><img src="${profile[i]}" alt="user profile"></li>
     <li class="playerName">${element.user}</li>
     <li class="playerScore">${element.score}</li>
     `;
-    // console.log(firstThree.height());
     legendSection.appendChild(firstThree);
   });
-  // console.log(legendSection.height());
-
-  
-  
 }
 
 const listScores = document.querySelector('.listScores');
 
-const addToDom = (name, score) => {
+const addToDom = (i, name, score) => {
   const player = document.createElement('ul');
   player.classList.add(`playerScoreList`);
-  player.classList.add('hidden');
   player.innerHTML += `
-    <li><img class="userProfile" src="${userProfile}" alt="user profile"></li>
+    <li><img class="userProfile" src="${profile[i]}" alt="user profile"></li>
     <li class="playerName">${name}</li>
     <li class="playerScore">${score}</li>
   `;
@@ -53,14 +44,15 @@ const addToDom = (name, score) => {
 };
 
 const listAllPlayers = async () => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7CEoFTu437FqyLNIZWMY/scores/');
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Qo64ntsg5rYTD1fJgVFE/scores/');
   const data = await response.json();
   data.result.sort((a,b) => b.score - a.score);
   console.log(data.result)
   legend(data.result.slice(0,3));
-  data.result.forEach((element) => {
-    addToDom(element.user, element.score);
-  });
+  data.result.forEach((element, i) => {
+    if (i > 3) { i = 3 };
+    addToDom(i, element.user, element.score);
+  })
 };
 
 submit.addEventListener('click', () => {
